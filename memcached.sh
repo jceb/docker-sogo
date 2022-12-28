bed76851de88ab8f2e7c0dab1c6e698d8b016a33
Jan Christoph Ebersbach <jceb@e-jc.de>
2022-12-28 15:38:57 +0100

    fix(memcache): bind only to localhost

--- memcached.sh
+++ memcached.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eo pipefail
 
-exec /sbin/setuser memcache /usr/bin/memcached -m ${memcached:-64} >>/var/log/memcached.log 2>&1
+exec /sbin/setuser memcache /usr/bin/memcached -l ::1 -m ${memcached:-64} >>/var/log/memcached.log 2>&1
